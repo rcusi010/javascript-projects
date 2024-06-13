0cead0890cb295c833215f66bfa197c12a4d5a8c
Roman C <romancusi010@gmail.com>
2024-06-12 21:02:18 -0500

    conditionals added

--- booleans-and-conditionals/exercises/part-2.js
+++ booleans-and-conditionals/exercises/part-2.js
@@ -4,6 +4,25 @@ let shuttleCabinReady = true;
 let crewStatus = spaceSuitsOn && shuttleCabinReady;
 let computerStatusCode = 200;
 let shuttleSpeed = 15000;
+if (crewStatus = true ){
+    console.log("Crew Ready");
+}else{
+    console.log("Crew Not Ready");
+}
+if (computerStatusCode = 200){
+    console.log("Plears stand by. Computer is rebooting");
+} else if (computerStatusCode = 400){
+    console.log("Sucess! Computer online");
+}else{
+    console.log("Alert! Computer Offline!");
+}
+if (shuttleSpeed > 17500){
+    console.log("ALERT: Escape velocity reached!");
+}else if (computerStatusCode < 8000){
+    console.log("ALERT: Cannot maintain orbit!");
+}else{
+    console.log("Stable speed")
+}
 
 // 3) Write conditional expressions to satisfy the following safety rules:
 
